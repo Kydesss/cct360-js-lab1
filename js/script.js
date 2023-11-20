5f684bccd749d224b771e2e1ab118721e9d29dfc
Kydesss <joaquin.pacia@mail.utoronto.ca>
2023-11-20 18:54:52 -0500

    Created calculateAge function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,9 @@
-var firstName, lastName, email, currentYear, birthYear;
-firstName = "Joaquin";
-lastName = "Pacia";
-email = "something@gmail.com";
-currentYear = 2023;
-birthYear = 2003;
+var currentYear = 2023;
 
-function myFunction() {
+function calculateAge() {
+    var name = document.getElementById("name");
+    var date = document.getElementById("date");
+    var age = currentYear - date.value.toString().split("-")[0];
     document.getElementById("customMessage").innerHTML =
-        "Hello " +
-        firstName +
-        " " +
-        lastName +
-        "!<br>" +
-        "You are currently " +
-        (currentYear - birthYear) +
-        " years old this year.";
+        "Hello " + name.value + " your age is " + age;
 }
